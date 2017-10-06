53de6fa21de5cea6d6565d3346928b9e6851859c
Ethan Harmon <ethanharmon2018@gmail.com>
2017-10-05 22:36:14 -0800

    Demo for 10/6 class

---
+++ Pizza Simulator/Assets/NextHouse7.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextHouse7 : MonoBehaviour {
+
+	public GameObject obj1;
+	public GameObject obj2;
+	public bool isCurrentWaypoint;
+	public NextHouse8 next;
+	public Collider coll;
+	public GameObject House;
+
+   void Start()
+   {
+	   obj1.SetActive (false);
+           obj2.SetActive (false);
+   }
+
+   // Toggle the Object's visibility if this house is the current waypoint.
+   void Update()
+   {
+	   if (isCurrentWaypoint == true)
+   	{
+   		obj1.SetActive (true);
+                   obj2.SetActive (true);
+   	} else {
+   		obj1.SetActive (false);
+                   obj2.SetActive (false);
+   	}
+	if(Input.GetKeyDown(KeyCode.Escape) == true)
+	{
+	   	Application.Quit();
+	}
+   }
+
+   void OnTriggerEnter(Collider coll) {
+	   isCurrentWaypoint = false;
+	   next.isCurrentWaypoint = true;
+   }
+
+}
---
+++ Pizza Simulator/Assets/NextHouse9.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextHouse9 : MonoBehaviour {
+
+	public GameObject obj1;
+	public GameObject obj2;
+	public bool isCurrentWaypoint;
+	public Collider coll;
+	public GameObject House;
+
+   void Start()
+   {
+	   obj1.SetActive (false);
+           obj2.SetActive (false);
+   }
+
+   // Toggle the Object's visibility if this house is the current waypoint.
+   void Update()
+   {
+	if (isCurrentWaypoint == true)
+   	{
+   		obj1.SetActive (true);
+                   obj2.SetActive (true);
+   	} else {
+   		obj1.SetActive (false);
+                   obj2.SetActive (false);
+   	}
+	if(Input.GetKeyDown(KeyCode.Escape) == true)
+	{
+	   	Application.Quit();
+	}
+   }
+
+   void OnTriggerEnter(Collider coll) {
+	   //gameController.GameOver();
+   }
+
+}
